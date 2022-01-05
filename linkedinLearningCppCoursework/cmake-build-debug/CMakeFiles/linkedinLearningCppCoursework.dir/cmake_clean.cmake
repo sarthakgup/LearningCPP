3a07efd27c18e4d22b4e0f7230a1485f5353e7cb
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-05 14:27:44 -0800

    new file for linkedinlearning code

---
+++ linkedinLearningCppCoursework/cmake-build-debug/CMakeFiles/linkedinLearningCppCoursework.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkedinLearningCppCoursework.dir/main.cpp.obj"
+  "liblinkedinLearningCppCoursework.dll.a"
+  "linkedinLearningCppCoursework.exe"
+  "linkedinLearningCppCoursework.exe.manifest"
+  "linkedinLearningCppCoursework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linkedinLearningCppCoursework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
