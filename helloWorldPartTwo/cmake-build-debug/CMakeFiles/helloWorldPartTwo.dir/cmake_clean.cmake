8bce5d7d5d96dfb98465d2bae1b7e6d050c2e238
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-05 14:03:58 -0800

    hello world program part two up and running now for testing

---
+++ helloWorldPartTwo/cmake-build-debug/CMakeFiles/helloWorldPartTwo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorldPartTwo.dir/main.cpp.obj"
+  "helloWorldPartTwo.exe"
+  "helloWorldPartTwo.exe.manifest"
+  "helloWorldPartTwo.pdb"
+  "libhelloWorldPartTwo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorldPartTwo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
