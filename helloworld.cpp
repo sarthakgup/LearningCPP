8de4ad8d4cf242d5fbdc4a43c7026bb418b9e8ba
Sarthak Gupta <16374350+sarthakgup@users.noreply.github.com>
2022-01-05 13:41:27 -0800

    testing workspace in vscode with simple helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Hello World! Enter your name: ";
+    cin >> str;
+    cout << "Nice to meet you, " << str << "!" << endl;
+    return(0);
+}
